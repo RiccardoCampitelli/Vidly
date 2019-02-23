d458b428126b4eddfa1ca184feb1048495733d33
rcdCamp <riccardo.campitelli91@gmail.com>
2019-02-23 16:45:58 +0000

    First Commmit

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+            
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
